63b92fd34fcd85ee7cbd27cca72e47a6f5ae5bfe
rizkirakasiwi <rizkirakasiwi09@gmail.com>
2020-02-19 13:15:54 +0700

    content provider done

---
+++ favoritemodule/build/generated/source/buildConfig/debug/com/example/favoritemodule/BuildConfig.java
@@ -0,0 +1,15 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.example.favoritemodule;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "com.example.favoritemodule";
+  public static final String BUILD_TYPE = "debug";
+  public static final String FLAVOR = "";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0";
+  // Fields from default config.
+  public static final String API_KEY = "4a286864a1858629d01785f0a55d4f90";
+}
